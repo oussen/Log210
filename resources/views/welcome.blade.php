271dcbb50d0f236924a0791c7228439dec6e4a95
twoTimesAgnew <am54760@ens.etsmtl.ca>
2015-09-24 15:04:50 -0400

    Created textboxes for username and password

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+	
+        <title>Login</title>
+    </head>
+    <body>
+        <div class="container">
+			<div class="col-md-8"></div>
+            <div class="content col-md-4" style="text-align=right; style=padding-top=10px !important;">
+				<div class="form-group">
+					<input type="text" class="form-control" id="usr" placeholder="Username">
+				</div>
+				<div class="form-group">
+					<input type="password" class="form-control" id="pwd" placeholder="Password">
+				</div>
+            </div>
+        </div>
+    </body>
+</html>
