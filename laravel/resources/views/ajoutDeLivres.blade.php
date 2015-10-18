80c93c9a5c228c4f8d1227584a3b741b17788aa0
twoTimesAgnew <vic.agnew@gmail.com>
2015-10-17 20:03:36 -0400

    Added master.blade.php and incorporated it in welcome.blade.php and ajoutDeLivres.blade.php

---
+++ laravel/resources/views/ajoutDeLivres.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 10/17/15
+ * Time: 7:39 PM
+ */
---
+++ laravel/resources/views/layouts/master.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 10/17/15
+ * Time: 6:31 PM
+ */
