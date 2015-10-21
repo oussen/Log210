ea336da9be5d961fc7208d543a28d53108423b06
beniiiii <am42310@ens.etsmtl.ca>
2015-10-21 08:31:21 -0400

    Avancement de l'ajout de livre

--- laravel/public/js/books.js
+++ laravel/public/js/books.js
@@ -2,7 +2,31 @@
  * Created by victor on 10/20/15.
  */
 $(document).ready(function() {
-
-    console.log("hello");
-
+	$("button").click(function(){
+		var ISBN = document.getElementById("ISBN").innerHTML();
+		$("td[contenteditable=true]").each(function(){
+			if($(this).attr("id") == "title")
+			{
+				var titre = $(this).text();
+			}
+			else if($(this).attr("id") == "authors")
+			{
+				var auteurs = $(this).text();
+			}
+			else if($(this).attr("id") == "pageCount")
+			{
+				var pageCount = $(this).text();
+			}
+			else if($(this).attr("id") == "price")
+			{
+				var prix = $(this).text();
+			}
+		});
+		
+		var query = "INSERT INTO livres VALUES (" + ISBN + ", " + titre + ", " + auteurs + ", " + pageCount + ", " + prix + ");"
+		console.log(query);
+		
+		document.getElementsByName("sqlQuery").innerHTML = query;
+	});
 });
+
--- laravel/resources/views/ajoutDeLivres.blade.php
+++ laravel/resources/views/ajoutDeLivres.blade.php
@@ -4,9 +4,87 @@
 
 @section('content')
     <div id="rechercheISBN">
-		<input class="typeahead" type="text" placeholder="Code ISBN">
+		<form action="ajoutDeLivres" method="GET">
+		<input class="txtLivres" type="text" id="ISBN" placeholder="Code ISBN">
+		<input type="submit">
+		</form>
 	</div>
 	
-	{!! Html::script('js/books.js', array('books' => 'books')) !!}
+	<input type="hidden" name="sqlQuery">
 	
+	<?php
+	
+	if(isset($_GET['ISBN'])){
+		if($_GET['ISBN'] != "")
+		{
+			$isbn = $_GET['ISBN'];
+			$curl = curl_init();
+			curl_setopt($curl, CURLOPT_URL,"https://www.googleapis.com/books/v1/volumes?q=+isbn:". $isbn . "&key=AIzaSyBbtI1mN-lvCzHQrCxVel47M9IF4I9udL0&fields=items/volumeInfo(title,authors,pageCount),items/saleInfo/retailPrice/amount");
+			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			$result = curl_exec($curl);
+			
+			$livre = json_decode($result, true);
+			
+			if(isset($livre['items'])){
+				
+				$titre = "";
+				$auteurs = "";
+				$pageCount = "";
+				$prix = "";
+				
+				$info_livre = $livre['items'][0];
+				curl_close($curl);
+				
+				if(isset($info_livre['volumeInfo']['title'])){
+					$titre = $info_livre['volumeInfo']['title'];
+				}
+			
+				if(isset($info_livre['volumeInfo']['authors'])){
+					$auteurs = $info_livre['volumeInfo']['authors'][0];
+				}
+			
+				if(isset($info_livre['volumeInfo']['pageCount'])){
+					$pageCount = $info_livre['volumeInfo']['pageCount'];
+				}
+			
+				if(isset($info_livre['volumeInfo']['retailPrice'])){
+					$prix = $info_livre['volumeInfo']['retailPrice']['amount'];
+				}
+				
+				echo 'Veuillez corriger toute imperfection dans les données recueillies.';
+				
+				echo '<table class="table">';
+				echo '<thead>';
+				echo '<tr>';
+				echo '<th>Titre</th>';
+				echo '<th>Auteur</th>';
+				echo '<th>Nombre de pages</th>';
+				echo '<th>Prix</th>';
+				echo '</tr>';
+				echo '</thead>';
+				echo '<tbody>';
+				echo '<tr class="odd">';
+				echo '<td id="title" contenteditable="true">' . $titre . '</td>';
+				echo '<td id="authors" contenteditable="true">' . $auteurs . '</td>';
+				echo '<td id="pageCount" contenteditable="true">' . $pageCount . '</td>';
+				echo '<td id="price" contenteditable="true">' . $prix . '</td>';
+				echo '</tr>';
+				echo '</tbody>';
+				echo '</table>';
+				
+				echo '<button type="button">Enregistrer</button>';
+			}
+			else
+			{
+				echo $result;
+			}
+		}
+	}
+	?>
+	
+@endsection
+
+@section('scripts')
+	{!! Html::script('js/books.js') !!}
 @endsection
